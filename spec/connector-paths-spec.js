7680f50237a03aba714ca60d3cbb78ac27e57a46
David de Florinier <dave@deflorinier.me.uk>
2016-10-26 15:56:52 +0100

    connector-paths + spec

---
+++ spec/connector-paths-spec.js
@@ -0,0 +1,77 @@
+/*global describe, beforeEach, it, expect, require*/
+describe('connectorPaths', function () {
+	'use strict';
+	var underTest = require('../src/connector-paths'),
+		calculatedConnector, position, parent, child;
+	beforeEach(function () {
+		calculatedConnector = {
+			from: {
+				x: 0,
+				y: 20
+			},
+			to: {
+				x: 100,
+				y: 30
+			},
+			connectorTheme: {
+				controlPoint: {
+					height: 1.25
+				}
+			}
+		};
+		position = {
+			top: 10,
+			left: 10
+		};
+		parent = {
+			height: 40
+		};
+		child = {
+			height: 30
+		};
+	});
+	describe('quadratic', function () {
+		it('should return quadratic path', function () {
+			expect(underTest.quadratic(calculatedConnector, position, parent, child)).toEqual({
+				d: 'M-10,10Q-10,33 90,20',
+				position: position
+			});
+		});
+	});
+	describe('vertical-quadratic-s-curve', function () {
+		beforeEach(function () {
+			calculatedConnector = {
+				from: {
+					x: 0,
+					y: 20
+				},
+				to: {
+					x: 30,
+					y: 100
+				}
+			};
+		});
+		it('should return quadratic s curve path', function () {
+			expect(underTest['vertical-quadratic-s-curve'](calculatedConnector, position, parent, child)).toEqual({
+				d: 'M-10,10q0,20 15,40q15,20 15,40',
+				position: position
+			});
+		});
+		describe('should return a straight line if connector to is below the connector from with tolerance', function () {
+			it('to the left', function () {
+				calculatedConnector.to.x = -19;
+				expect(underTest['vertical-quadratic-s-curve'](calculatedConnector, position, parent, child)).toEqual({
+					d: 'M-10,10l-19,80',
+					position: position
+				});
+			});
+			it('to the right', function () {
+				calculatedConnector.to.x = 19;
+				expect(underTest['vertical-quadratic-s-curve'](calculatedConnector, position, parent, child)).toEqual({
+					d: 'M-10,10l19,80',
+					position: position
+				});
+			});
+		});
+	});
+});
