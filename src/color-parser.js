3a2dce158e1f2254ea5fbced25aceaa643f8a200
David de Florinier <dave@deflorinier.me.uk>
2016-11-04 08:53:42 +0000

    1.10.4

---
+++ src/color-parser.js
@@ -0,0 +1,13 @@
+/*global module, require*/
+var convertToRGB = require('./color-to-rgb');
+module.exports = function colorParser(colorObj) {
+	'use strict';
+	if (!colorObj.color || colorObj.opacity === 0) {
+		return 'transparent';
+	}
+	if (colorObj.opacity) {
+		return 'rgba(' + convertToRGB(colorObj.color).join(',') + ',' + colorObj.opacity + ')';
+	} else {
+		return colorObj.color;
+	}
+};
